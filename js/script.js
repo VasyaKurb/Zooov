3fd91512ef875cb8f3d702a73d961ed75bc16e3b
VasyaKurb <xtdyfydyfyf6@gmail.com>
2021-12-10 22:30:36 +0300

    burger

--- js/script.js
+++ js/script.js
@@ -20,9 +20,29 @@ for (var i = 0; i < faqControl.length; i++) {
 /* -------------------------------------------------------------------- */
 
 // JQuery
+// thank-popup
+$(function(){
+	$('#th-open-btn').click(function() {
+		$('.thank-popup').addClass('active');
+	});
+
+	$('#th-x, #overlay').click(function() {
+		$('.thank-popup').removeClass('active');
+	});
+});
+
+// Слайдер
+$(function(){
+  	$('.burger').click(function() {
+        $('.burger,.header__menu').toggleClass('menu-active');
+        $('body').toggleClass('lock');
+    });
+});
+
 $(function(){
   	$('.product__slider_wrap').slick({
   		infinite: true,
+  		dots: false,
   		arrows: false,
   		slidesToShow: 2,
   		responsive: [
@@ -40,26 +60,29 @@ $(function(){
 $(function() {
 	$('.rations__items').slick({
 		arrows: false,
-        dots: true,
 		infinite: false,
+		dots: false,
 		slidesToShow: 4,
 		responsive: [
 		    {
 		      breakpoint: 769,
 		      settings: {
 		        slidesToShow: 3,
+		        dots: true,
 		      }
 		    },
 		    {
 		    	breakpoint: 651,
 		    	settings: {
 		    		slidesToShow: 2,
+			        dots: true,
 		    	}
 		    },
 		    {
 		    	breakpoint: 456,
 		    	settings: {
 		    		slidesToShow: 1,
+			        dots: true,
 		    	}
 		    }
 		]
